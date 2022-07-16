297c9549d9dd707eda1996876d4d4047a6091073
DESKTOP-8V4V0GG\User <shhwangbimatrix@gmail.com>
2022-07-17 02:35:51 +0900

    20220711 과제2 오타 수정

--- 20220711 code_2.py
+++ 20220711 code_2.py
@@ -31,7 +31,7 @@ df1
 
 # %%
 ## array1
-df2 = pd.DataFrame(np.array([[1,2,3,], [4,5,6], [7,8,9]]), columns=['a','b','c'])
+df2 = pd.DataFrame(np.array([[1,2,3], [4,5,6], [7,8,9]]), columns=['a','b','c'])
 df2
 
 # %%
