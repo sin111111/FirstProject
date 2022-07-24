8d139e6531f5fd37e7a8005711f0828fa6430396
DESKTOP-8V4V0GG\User <shhwangbimatrix@gmail.com>
2022-07-24 14:18:17 +0900

    20220711 과제4 수정해서 업로드

--- 20220711 code_4.py
+++ 20220711 code_4.py
@@ -6,7 +6,6 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-# %%
 init_url = 'https://www.kovo.co.kr/stats/42001_team-totalrecord.asp'
 
 html = requests.get(init_url, verify=False)
@@ -15,7 +14,6 @@ if(html.status_code == 200):
     soup = BeautifulSoup(html.content, 'html.parser')
     html.close()
 
-# %%
 # 모든 파라미터의 변수명을 알기 위해서 기록보기 버튼을 클릭하면
 # url = 'https://www.kovo.co.kr/stats/42001_team-totalrecord.asp?s_part=1&spart=&s_season=016&s_pr=201%7C3&e_season=017&e_pr=201%7C1&part=point' 를 얻게 된다.
 # 해당 페이지의 변수는 s_part, spart, s_season, s_pr, e_season, e_pr, part 총 7개이다.
@@ -24,23 +22,14 @@ if(html.status_code == 200):
 
 param_names = ['s_season', 's_pr', 'e_season', 'e_pr', 'part']
 
-# %%
-dict = {}
-for name in param_names:
-    selects = soup.find('select', {'name': name})
+def get_param_info(param_name):
+    options = soup.find('select', {'name': param_name}).find_all('option')
     temp_dict = {}
-    for i, option in enumerate(selects):
-        if i % 2 != 0: # 이해 조금 어려움...
-            option_value = str(option).split('value="')[1].split('">')[0]
-            temp_dict.setdefault(option_value,option.get_text())
+    for option in options:
+        value = option.attrs['value']
+        temp_dict[value] = option.get_text()
+    
+    return temp_dict
+    
 
-    dict.setdefault(name, temp_dict)
-
-
-for name in param_names:
-    print(name + " ::: " + str(dict[name]))
-
-# %%
-
-
-
+get_param_info('s_season')
