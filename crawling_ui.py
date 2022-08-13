7320e6fa7a93f8fd4f3206e6ff2f77341269779e
DESKTOP-8V4V0GG\User <shhwangbimatrix@gmail.com>
2022-08-13 15:42:22 +0900

    크롤링 GUI 중간 업로드

---
+++ crawling_ui.py
@@ -0,0 +1,39 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QTextEdit
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+
+        btn1 = QPushButton('&Button1', self)
+        btn1.setText('crawling 실행')
+        btn1.clicked.connect(self.btn1clicked)
+
+        self.te = QTextEdit()
+        self.te.setAcceptRichText(False)
+        self.setWindowTitle('Crawling FisrtProject')  # self: MyApp 객체
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.te)
+        hbox.addWidget(btn1)
+        hbox.addStretch()
+
+        self.setLayout(hbox)    
+
+        self.move(300,300)
+        self.resize(500, 200)
+        self.show()
+
+    def btn1clicked(self):
+        self.te.text = "aaa"
+
+
+    
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
